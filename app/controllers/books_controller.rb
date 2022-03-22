bcfbb878c540d5915471403eb1ba2a4526b413d6
EC2 Default User <ec2-user@ip-172-31-7-175.ec2.internal>
2022-03-22 11:21:43 +0000

    [add]マイページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,15 +11,20 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
   end
     
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-root to: "homes#top"
 devise_for :users
-resources :books,only: [:new, :create, :index, :show, :edit, :destroy]
+root to: "homes#top"
 get "/homes/about" => "homes#about", as: "about"
+resources :books,only: [:new, :create, :index, :show, :edit, :destroy]
+resources :users, only: [:show, :edit]
 end
