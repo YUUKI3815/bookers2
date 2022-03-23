8cf1ee75313f7bc1697c32e8213f1c15f2967df3
EC2 Default User <ec2-user@ip-172-31-7-175.ec2.internal>
2022-03-23 07:09:03 +0000

    [add]introduction 保存

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ devise_for :users
 root to: "homes#top"
 get "/homes/about" => "homes#about", as: "about"
 resources :books,only: [:new, :create, :index, :show, :edit, :destroy]
-resources :users, only: [:show, :edit]
+resources :users,only: [:show, :edit, :update]
 end
