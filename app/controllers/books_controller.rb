12f2c0c444de684c355af75bc5c8994dbc013810
EC2 Default User <ec2-user@ip-172-31-7-175.ec2.internal>
2022-03-28 05:00:44 +0000

    [add]他人のページを編集できない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,13 +28,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
      @book = Book.find(params[:id])
    if @book.update(book_params)
     redirect_to book_path(@book.id)
-    flash[:notice] = "You have updated book successfully"
+    flash[:notice] = "You have updated book successfully."
    else
     render :edit
    end
