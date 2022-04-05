29c969b8fb0a7ef60e2b1da9787d0a1dfdc39447
YUUKI3815 <s.yuuki3815@gmail.com>
2022-04-05 04:31:36 +0000

    [add]コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ devise_for :users
 root to: "homes#top"
 get "/home/about" => "homes#about", as: "about"
 resources :books,only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+ resources :book_comments, only: [:create, :destroy]
  resource :favorites, only: [:create, :destroy]
  end
 resources :users,only: [:index,:show, :edit, :update]
